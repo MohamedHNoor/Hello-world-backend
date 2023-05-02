9a4b3a610c59beee7475449c8b472cdd7f516231
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-02 14:09:13 +0200

    Add greetings controller for the api and pages controller

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        greeting = Greeting.all.sample.slice(:text)
+        render json: {greeting: greeting}
+      end
+    end
+  end
+end
